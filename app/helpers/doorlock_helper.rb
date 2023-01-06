698061eaa5d349ac861bfaef87ae7e55d0c2a609
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2023-01-06 11:17:36 +0100

    Add the necessary helpers to display relevant information on the UI

--- app/helpers/doorlock_helper.rb
+++ app/helpers/doorlock_helper.rb
@@ -1,2 +1,28 @@
 module DoorlockHelper
+  def lock_state(last_lock_state)
+    if last_lock_state.updated_at < 2.minutes.ago
+      'error'
+    else
+      last_lock_state.lock_state
+    end
+  end
+
+  def background_color(last_lock_state)
+    case last_lock_state
+    when 'open'
+      'bg-warning'
+    when 'locked'
+      'bg-info'
+    else
+      'bg-danger'
+    end
+  end
+
+  def translate(last_lock_state)
+    if last_lock_state.updated_at < 2.minutes.ago
+      I18n.t('states.error', timestamp: Date.strptime(last_lock_state.updated_at, '%F %R'))
+    else
+      I18n.t "states.#{last_lock_state.lock_state}"
+    end
+  end
 end
--- spec/helpers/doorlock_helper_spec.rb
+++ spec/helpers/doorlock_helper_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe DoorlockHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:helper) { Class.new }
+  before { helper.extend DoorlockHelper }
+
+  describe '#lock_state' do
+    subject(:lock_state) { helper.lock_state(last_lock_state) }
+
+    context 'when the last database entry is older than 2 minutes' do
+      let(:last_lock_state) { OpenStruct.new(updated_at: 3.minutes.ago.to_s) }
+
+      it { is_expected.to eq 'error' }
+    end
+
+    context 'when the last database entry was within 2 minutes' do
+      let(:last_lock_state) { OpenStruct.new(lock_state: 'open', updated_at: 1.minute.ago.to_s) }
+
+      it 'returns the last lock state' do
+        expect(lock_state).to eq last_lock_state.lock_state
+      end
+    end
+  end
+
+  describe '#background_color' do
+    context 'when door is open' do
+      it 'sets the background color to warning' do
+        expect(helper.background_color('open')).to eq 'bg-warning'
+      end
+    end
+
+    context 'when door is locked' do
+      it 'sets the background color to info' do
+        expect(helper.background_color('locked')).to eq 'bg-info'
+      end
+    end
+
+    context 'when no data received' do
+      it 'sets the background color to danger' do
+        expect(helper.background_color('No data received...')).to eq 'bg-danger'
+      end
+    end
+  end
+
+  describe '#translate' do
+    subject(:translation) { helper.translate(last_lock_state) }
+
+    context 'when no data received' do
+      let(:timestamp) { 3.minutes.ago.to_s }
+      let(:last_lock_state) { OpenStruct.new(lock_state: 'open', updated_at: timestamp) }
+      it { is_expected.to eq "Missing status updates since #{Date.strptime(timestamp, '%F %R')}" }
+    end
+
+    context 'when door is open' do
+      let(:last_lock_state) { OpenStruct.new(lock_state: 'open', updated_at: 1.minute.ago.to_s) }
+      it { is_expected.to eq 'open' }
+    end
+
+    context 'when door is locked' do
+      let(:last_lock_state) { OpenStruct.new(lock_state: 'locked', updated_at: 1.minute.ago.to_s) }
+      it { is_expected.to eq 'locked' }
+    end
+  end
 end
