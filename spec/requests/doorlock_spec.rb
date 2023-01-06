2cb5c591fa55bc98a37bce72e5d530b548aaab6d
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2023-01-06 14:43:07 +0100

    Fix request spec

--- spec/requests/doorlock_spec.rb
+++ spec/requests/doorlock_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'Doorlocks', type: :request do
   include_context 'dynamodb user'
 
   describe 'GET /' do
+    let(:lock_state) { { item: { lock_state: 'locked', updated_at: Time.now } } }
+    before { allow(StatusStorage).to receive(:read_state).and_return lock_state }
+
     it 'returns http success' do
       get '/'
       expect(response).to have_http_status(:success)
