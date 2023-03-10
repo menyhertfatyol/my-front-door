38113e8e2b89fbf6830b07b25f62319ce064ba99
menyhertfatyol <33250442+menyhertfatyol@users.noreply.github.com>
2023-03-10 14:38:56 +0100

    Disable asset pipeline
    out of curiosity

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module MyFrontDoor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.assets.enabled = false
 
     # Don't generate system test files.
     config.generators.system_tests = nil
